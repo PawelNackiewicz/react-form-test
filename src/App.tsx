47817adf48d307815a1654ad5e30bc2fc0b334d5
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-10-15 15:28:11 +0200

    :tada: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen min-w-screen bg-purple-500 flex justify-center items-center">
+      <p className="text-9xl text-white tracking-wider">Hello App</p>
     </div>
   );
 }
-
-export default App;
